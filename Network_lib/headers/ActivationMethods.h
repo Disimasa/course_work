195e259ceb947c87238901237dd0128cbab63d38
Disimasa <lichko2002@mail.ru>
2020-12-28 23:09:37 +0300

    added relu func

--- Network_lib/headers/ActivationMethods.h
+++ Network_lib/headers/ActivationMethods.h
@@ -6,4 +6,9 @@ static double sigmoid(double x, bool derivative) {
 	return derivative ? x*(1-x) : 1/(1+exp(-x));
 }
 
+static double relu(double x, bool derivative) {
+	if (x == 0) return 0;
+	else return derivative ? 1 : x;
+}
+
 #endif //NEURAL_NETWORK_ACTIVATIONMETHODS_H
