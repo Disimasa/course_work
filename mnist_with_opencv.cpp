864e681f9a41032cfbf303ea3e2666782a4640ec
Disimasa <lichko2002@mail.ru>
2020-12-28 19:31:15 +0300

    added gitignore

--- mnist_with_opencv.cpp
+++ mnist_with_opencv.cpp
@@ -3,24 +3,30 @@
 #include <opencv2/core/eigen.hpp>
 
 int main() {
-	cv::Mat img = cv::imread("ex.png", cv::IMREAD_GRAYSCALE);
+	cv::Mat img = cv::imread("four.jpg", cv::IMREAD_GRAYSCALE);
 	cv::resize(img, img, cv::Size(28, 28));
 	Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> matrix;
 	cv::cv2eigen(img, matrix);
 	Matrix testData(1, 28*28);
 	for (int row = 0; row < 28; row++) {
 		for (int col = 0; col < 28; col++) {
-			testData(row*28 + col) = matrix(row, col);
+			if (matrix(row, col) > 150) {
+				matrix(row, col) = 0;
+				testData(row * 28 + col) = 0;
+			} else {
+				testData(row * 28 + col) = (255 - matrix(row, col)) / 255.0;
+			}
 		}
 	}
-	std::cout<<testData<<std::endl<<std::endl;
-//	Network network;
-//	network.addLayer(new Convolutional(28, 28, 3));
-//	network.addLayer(new FullyConnected(676));
-//	network.addLayer(new FullyConnected(10));
-//	network.readWeights("./weights.txt");
+//	std::cout<<testData<<std::endl<<std::endl;
+	std::cout<<matrix<<std::endl<<std::endl;
+	Network network;
+	network.addLayer(new Convolutional(28, 28, 3));
+	network.addLayer(new FullyConnected(676));
+	network.addLayer(new FullyConnected(10));
+	network.readWeights("./new_weights.txt");
 //
-//	std::cout<<network.activate(matrix).back();
+	std::cout<<network.activate(testData).back();
 //	cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
 //	imshow("Display window", img);
 	cv::waitKey(0);
