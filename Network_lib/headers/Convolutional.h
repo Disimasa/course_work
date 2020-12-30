2e1364c7a1e987b35b4bde1ba3cf132c215ebe32
Disimasa <lichko2002@mail.ru>
2020-12-30 05:23:32 +0300

    added some comments

--- Network_lib/headers/Convolutional.h
+++ Network_lib/headers/Convolutional.h
@@ -1,9 +1,12 @@
 #ifndef NEURAL_NETWORK_CONVOLUTIONAL_H
 #define NEURAL_NETWORK_CONVOLUTIONAL_H
 #include "Layer.h"
-
+/// Класс сверточного слоя нейронной сети
+///
+/// Чаще всего используется для обработки изображений, данная реализация использует valid метод для обработки изображения
 class Convolutional: public Layer {
 public:
+	/// Конструктор принимает размер изображения, размер ядра и функцию активации
 	Convolutional(int width, int height, int kernelSize = 3, double (*activationMethod)(double x, bool derivative) = sigmoid);
 	void setRandomWeights(Layer *nextLayer);
 	Matrix forward(Matrix input);
--- Network_lib/headers/FullyConnected.h
+++ Network_lib/headers/FullyConnected.h
@@ -1,9 +1,12 @@
 #ifndef NEURAL_NETWORK_FULLYCONNECTED_H
 #define NEURAL_NETWORK_FULLYCONNECTED_H
 #include "Layer.h"
-
+/// Класс полносвязного слоя нейронной сети
+///
+/// Используется для создания перцептронов разной топологии
 class FullyConnected:public Layer {
 public:
+	/// Конструктор принимает количество нейронов в слое и функцию активации
 	FullyConnected(int neuronsAmountVal, double (*activationMethodLink)(double x, bool derivative) = sigmoid);
 	void setRandomWeights(Layer *nextLayer);
 	Matrix forward(Matrix input);
